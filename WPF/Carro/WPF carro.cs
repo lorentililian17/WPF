f876078480ff39b77600690d2d9f03c9da227482
lorentililian17 <llorentip@miumg.edu.gt>
2022-04-23 23:23:31 -0600

    Agregar archivos de proyecto.

---
+++ WPF/Carro/WPF carro.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Carro
+{
+          public class CARRO
+
+          {
+            public string Marca { get; }
+            public int Modelo { get; }
+            public string Color { get; set; }
+            public int VMaxima { get; }
+
+            private int Encendido = 0;
+            private int velocidad_actual = 0;
+            private const int MAXVELOCIDAD = 150;
+
+            public CARRO(string _marca, int _modelo, string _color, int _vmaxima)
+            {
+                Marca = _marca;
+                Modelo = _modelo;
+                Color = _color;
+                VMaxima = _vmaxima;
+                Encendido = 0;
+                this.velocidad_actual = 0;
+            }
+            public string GetVelocidadActual()
+            {
+                return $"vamos a {velocidad_actual} KM/H";
+            }
+
+           public string EncendidoMotor()
+            {
+                if (Encendido == 0)
+                {
+                Encendido = 1;
+                return "Tu carro esta listo para correr";
+                    this.velocidad_actual = 0;
+                }
+                else
+                {
+                    return"Ya esta encendido tu carro!!!";
+                }
+            }
+
+            public string acelerar()
+            {
+                string mensaje = "";
+                if (Encendido == 0)
+                {
+                    mensaje = $"no puedo acelerar con el carro apagado";
+                    return mensaje;
+                }
+
+                velocidad_actual += 20;
+                if (velocidad_actual <= VMaxima)
+                {
+                    mensaje = $"Vas a {velocidad_actual} KPH";
+                }
+                else
+                {
+                    velocidad_actual = VMaxima;
+                    mensaje = $"{velocidad_actual}KPH Aguas rapido y furioso, vas muy rapido y ya llegaste al tope";
+                }
+
+                Console.WriteLine(mensaje);
+                return mensaje;
+
+            }
+
+            public string acelerar(int CuantosKPH)
+            {
+                string mensaje = "";
+                if (Encendido == 0)
+                {
+                    mensaje = $"No puedo acelerar con el carro apagado";
+                    return mensaje;
+
+                }
+
+                velocidad_actual += CuantosKPH;
+                if (velocidad_actual <= MAXVELOCIDAD)
+                {
+                    mensaje = $"Vas a {velocidad_actual} KM/H";
+                }
+                else
+                {
+                    velocidad_actual = MAXVELOCIDAD;
+                    mensaje = $"{velocidad_actual} KM/H Aguas toreto, Vas muy rapido y ya llegaste a; tope";
+                }
+                Console.WriteLine(mensaje);
+                return mensaje;
+            }
+
+            public string desacelerar()
+            {
+                string mensaje = "";
+                if (Encendido == 0)
+                {
+                    mensaje = $"No puedes desacelerar si el carro esta apagado!!";
+                    return mensaje;
+                }
+                else
+                {
+                    velocidad_actual -= 5;
+                    mensaje = $"Tu carro va desacelerando, tu velocidad actual es {velocidad_actual}";
+
+                }
+                Console.WriteLine(mensaje);
+                return mensaje;
+
+            }
+
+            public string frenar()
+            {
+                string mensaje = "";
+                if (Encendido == 0)
+                {
+                    mensaje = "El carro esta apagado, no puedes frenar";
+                }
+                else
+                {
+                    velocidad_actual -= 10;
+                    if (velocidad_actual >= 0)
+                    {
+                        mensaje = $"Tu carro va frenando, tu velocidad es {velocidad_actual}KM/H";
+
+                    }
+                    else
+                    {
+                        mensaje = $"Ya no puedes seguir frenando, vamos a 0KM/H";
+                    }
+                }
+
+
+                Console.WriteLine(mensaje);
+                return mensaje;
+            }
+
+            public string apagarmotor()
+            {
+                if (Encendido == 1)
+                {
+                    return"Tu Carro esta siendo apagado";
+                    return"Tu carro ya está apagado";
+                    Encendido = 0;
+                }
+                else
+                {
+                    return"Weii! el carro ya estaba apagado";
+                }
+            }
+
+            public string Bocinar()
+            {
+                string mensaje = "tututu!!! tu carro esta bocinando!";
+                Console.WriteLine(mensaje);
+                return mensaje;
+            }
+        }
+    }
+
+
---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Media;
+using WPF.Carro;
+
+namespace WPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CARRO carrito;
+        public MainWindow()
+        {
+            carrito = new CARRO("Mercedez Bens", 2022, "rojo", 150);
+            InitializeComponent();
+        }
+
+        private void buttonencender_Click(object sender, RoutedEventArgs e)
+        {
+            labelencender.Content = carrito.EncendidoMotor();
+        }
+
+        private void buttonacelerador_Click(object sender, RoutedEventArgs e)
+        {
+            labelacelerador.Content = carrito.acelerar();
+        }
+
+        private void buttonbocina_Click(object sender, RoutedEventArgs e)
+        {
+            labelbocina.Content = carrito.Bocinar();
+        }
+
+
+
+        private void buttonfrenar_Click(object sender, RoutedEventArgs e)
+        {
+            labelfrenar.Content = carrito.frenar();
+        }
+
+        private void buttonapagado_Click(object sender, RoutedEventArgs e)
+        {
+            labelapagado.Content = carrito.apagarmotor();
+        }
+
+        private void buttonradio_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Tu radio tiene muy buen sonido");
+        }
+
+     
+    }   
+}      
